c20c083ca50b3b2fdf8287cc4d5a1e4fc94ae2f6
Pádraig O'Rúis <prushguitar@gmail.com>
2023-10-05 00:29:34 +0100

    Add function to randomly select Computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+// computer selection
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"]
+    return options[Math.floor(Math.random() * options.length)]
+}
+
+console.log (getComputerChoice())
+
+// player selection
+
+// play a round of rps
+
+// play a full match
