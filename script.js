022ba006b853aa50009de64a7def417cd74546e9
Pádraig O'Rúis <prushguitar@gmail.com>
2023-10-05 01:23:40 +0100

    Fix addition assignment operator

--- script.js
+++ script.js
@@ -19,28 +19,28 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "rock") {
         console.log("You both picked rock, try again!")
     } else if (playerSelection == "rock" && computerSelection == "paper"){
-        computerScore =+ 1
+        computerScore += 1
         console.log("Paper covers rock, you lose!")
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore =+ 1
+        playerScore += 1
         console.log("Rock smashes scissors, you win!")
     } 
     // playerSelection = "paper"
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        playerScore =+ 1
+        playerScore += 1
         console.log("Paper covers rock, you win!")
     } else if (playerSelection == "paper" && computerSelection == "paper"){
         console.log("You both picked paper, try again!")
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        computerScore =+ 1
+        computerScore += 1
         console.log("Scissors cuts paper, you lose!")
     }
     // playerSelection = "scissors"
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        computerScore =+ 1
+        computerScore += 1
         console.log("Rock smashes scissors, you lose!")
     } else if (playerSelection == "scissors" && computerSelection == "paper"){
-        playerScore =+ 1
+        playerScore += 1
         console.log("Scissors cuts paper, you win!")
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         console.log("You both picked scissors, try again!")
